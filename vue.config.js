83f6de093d4262a55f8e02b768079b5b2596161d
Joanne Longeville <joanne.longeville@gmail.com>
2024-07-10 17:18:13 +0200

    modify package.json and vue.config.js to deploy with render

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    disableHostCheck: true,
+    public: 'sportfirst-front.onrender.com',
+    port: 8080,
+    host: '0.0.0.0'
+  }
 })
 
