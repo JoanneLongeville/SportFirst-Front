2321512046eec67e8bf3932f4c6196859b59301b
Joanne Longeville <joanne.longeville@gmail.com>
2024-07-11 16:48:07 +0200

    delete Profile in navbar for isSessionPage

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
         <span style="margin-left: 5px;font-family: 'Crete Round', sans-serif;font-size: 25px ">SportFirst <span style="color: #FFD700;font-size:40px">.</span></span>
   
         <div class="nav-links nav-links-right">
-          <router-link to="/profile-page" class="nav-link">Profil</router-link>
           <router-link to="#" class="nav-link" @click="showReservationsPopup">Mes réservations</router-link>
           <router-link to="/" class="nav-link" @click="logout">Se déconnecter</router-link>
         </div>
